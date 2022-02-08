270fea4bae3e89f2c9ab63a3c7ec04bb4c602de0
NasigariJitesh <72294952+NasigariJitesh@users.noreply.github.com>
2022-02-08 12:44:16 +0530

    heroku

--- src/app.js
+++ src/app.js
@@ -4,10 +4,27 @@ const adminRouter = require(`./routes/adminRoutes`);
 const organisationsRouter = require(`./routes/organisationsRoutes`);
 const mongoSanitize = require("express-mongo-sanitize");
 const erroHandler = require("./controllers/errorController");
+const dotenv = require('dotenv');
+dotenv.config({ path: `${__dirname}/config.env` });
 
 const cors = require("cors");
 
 const app = express();
+const mongoose = require('mongoose');
+
+const PORT = process.env.PORT || 3000;
+const dbUrl = process.env.DB_URL.replace('<password>', process.env.Password);;
+//database connect
+mongoose.connect(dbUrl, {
+  useNewUrlParser: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('connected');
+}).catch((err) => {
+  console.log(err);
+})
 
 app.use(cors());
 app.use(express.json());
@@ -18,5 +35,14 @@ app.use('/api/listings', organisationsRouter);
 app.use('/api/admin', adminRouter);
 
 app.use(erroHandler)
+
+
+
+
+
+app.listen(PORT, (err) => {
+  if (err) console.log(`Error in starting main-app server at ${PORT}`);
+  console.log(`main-app server started at ${PORT}`);
+})
 
 module.exports = app;
