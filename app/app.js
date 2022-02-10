a6390ff090ed5e8a658bde5cbaf9926368b4356e
NasigariJitesh <72294952+NasigariJitesh@users.noreply.github.com>
2022-02-10 14:21:29 +0530

    cors

--- app/app.js
+++ app/app.js
@@ -26,8 +26,15 @@ mongoose.connect(dbUrl, {
   console.log(err);
 })
 
+
 app.use(express.json());
 app.use(mongoSanitize());
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    next();
+})
 
 app.use('/api/users',userRouter);
 app.use('/api/listings', organisationsRouter);
