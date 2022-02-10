781b5b3fbced289fd02b28602fba0295ce6f43d6
NasigariJitesh <72294952+NasigariJitesh@users.noreply.github.com>
2022-02-10 14:10:30 +0530

    cors

--- app/app.js
+++ app/app.js
@@ -43,10 +43,9 @@ var corsOptions = {
   credentials: true
 }
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
   });
 app.use(express.json());
