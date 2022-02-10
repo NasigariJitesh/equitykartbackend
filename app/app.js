2d0d2128fb383be6f9042fbf0be6b7e34dd2ca55
NasigariJitesh <72294952+NasigariJitesh@users.noreply.github.com>
2022-02-10 14:27:12 +0530

    cors

--- app/app.js
+++ app/app.js
@@ -31,8 +31,10 @@ app.use(express.json());
 app.use(mongoSanitize());
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Credentials", "true");
+  res.setHeader("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT,PATCH");
+  res.setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers");
     next();
 })
 
